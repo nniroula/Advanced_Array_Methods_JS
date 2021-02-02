2f8cda9f52abeed7ded5f31483f69c0670e90366
Nabin <nkbhaie2017@gmail.com>
2021-02-02 05:59:40 -0700

    add callback, function expression

---
+++ advancedArr.js
@@ -0,0 +1,12 @@
+// Basic functions in js
+function myName(){
+    console.log("Hello my name is Nabin");
+}
+myName();
+
+//function expression
+// we assign a function to a variable and call that variable as a function.
+const myName2 = function(){
+    console.log("Hello my name is Nabin");
+}
+myName2();
